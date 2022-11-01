108161d73abed7faa75b7132c259d412427d56b7
Albert Johannessen <albertjohannessen@live.no>
2022-11-01 03:32:44 +0100

    Working deep dreams. Updated Readme

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-# deep_dream
+# Deep Dreaming
 
 Implementation of deep dreaming in PyTorch.
 Based on the original [blog](https://ai.googleblog.com/2015/06/inceptionism-going-deeper-into-neural.html) and [repo](https://github.com/google/deepdream) from Google Research.
+
+Uses a VGG19 model pretrained on the ImageNet-1000 dataset.
+
+Deep CNN models are trained to recognize certain features in the training set.
+Because the training set has a finite number of data and data classes
+it means that when an image is sent through the network it will be
+interpreted using the existing training of the model.
+
+This can be exploited to generate some interesting looking images.
+
+<div align="center">
+  <img src="Images/JC.jpg" width="300">
+  <img src="DreamOutput/JC.jpg" width="300">
+  <p>Input on the left, deep dreamed on the right.</p> 
+</div>
+
+An image sent through the network is stopped at a certain layer
+corresponding to a certain level of abstraction.
+The network might see some features that resembles things from the training set.
+These features are then amplified in the input image using gradient ascent
+with gradients computed using PyTorch autograd from the MSE loss of the features.
+
+The original implementation also does some more tricks to get better image outputs:
+* Add random offsets to the image at each epoch. This confuses the model and results in it seeing more features.
+* Normalizing the gradients making the iterations more stable.
+* Ascend at different octaves. An octave in this context refers to a scaled version of the input image.
+
--- deep_dream.py
+++ deep_dream.py
@@ -5,10 +5,10 @@ import numpy as np
 from tqdm import tqdm
 import torch
 import torch.nn as nn
-from torch.optim import Adam
 from torchvision.models import vgg19, VGG19_Weights
 from torchvision.io import read_image
 from torchvision.utils import save_image
+from torchvision.transforms.functional import resize
 
 seed = 42069
 random.seed(seed)
@@ -22,90 +22,109 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Model(nn.Module):
 
-    def __init__(self, pretrained_model, feature_layers):
+    def __init__(self, pretrained_model, final_layer):
         super().__init__()
         self.pretrained_model = pretrained_model
-        self.feature_layers = feature_layers
+        self.final_layer = final_layer
 
     def forward(self, x):
-        features = []
         for i, layer in enumerate(self.pretrained_model.features):
             x = layer(x)
-            if i in self.feature_layers:
-                features.append(x)
-            if i == self.feature_layers[-1]:
-                break
-        return features
+            if i == self.final_layer:
+                return x
 
 
-def dream():
-    image_name = sys.argv[1]
-    image_path = os.path.abspath(image_name)
-    image = read_image(image_path).float() / 255
-    image = image.unsqueeze(0)
-    # image = torch.rand(image.shape)
-    image = image.to(device)
+def ascend(image, model, lower_clamp, upper_clamp, step_size=0.05, jitter=32):
+    with torch.no_grad():
+        shift_y, shift_x = torch.randint(-jitter, jitter + 1, (2,))
+        image = torch.roll(image, shifts=(shift_y, shift_x), dims=(2, 3))
+        image.requires_grad = True
+
+    L = 0
+    features = model(image)
+    for feature in features:
+        L += torch.mean(feature**2)
+    L.backward()
+
+    with torch.no_grad():
+        image += step_size / torch.mean(torch.abs(image.grad)) * image.grad
+        image.grad = None
+
+        image = torch.roll(image, shifts=(-shift_y, -shift_x), dims=(2, 3))
+        image.clamp_(lower_clamp, upper_clamp)
+
+    return image
+
+
+def deepdream(image, model, preprocess, epochs=10, octaves=4, octave_zoom=1.4):
+    p_std = torch.tensor(preprocess.std)
+    p_mean = torch.tensor(preprocess.mean)
+    p_std = torch.reshape(p_std, (1, 3, 1, 1)).to(device)
+    p_mean = torch.reshape(p_mean, (1, 3, 1, 1)).to(device)
+
+    lower_clamp = -p_mean / p_std
+    upper_clamp = (1 - p_mean) / p_std
+
+    image_octaves = [preprocess(image)]
+    for i in range(octaves):
+        sh, sw = image_octaves[-1].shape[2:4]
+        sh = int(sh // octave_zoom)
+        sw = int(sw // octave_zoom)
+        image_octave = resize(image_octaves[-1], (sh, sw))
+        image_octaves.append(image_octave)
+
+    detail = torch.zeros(image_octaves[-1].shape)
+    for i, image_octave in enumerate(reversed(image_octaves)):
+        if i > 0:
+            sh, sw = image_octave.shape[2:4]
+            detail = resize(detail, (sh, sw))
+
+        input = image_octave + detail
+        for epoch in tqdm(range(1, epochs + 1)):
+            input = ascend(input, model, lower_clamp, upper_clamp)
+
+        detail = input - image_octave
+
+    image = input
+
+    # Deprocess
+    image = image * p_std + p_mean
+    image = torch.clamp(image, 0, 1)
+
+    return image
+
+
+def dream(random=False):
+    if random:
+        image = torch.rand((1, 3, 300, 300))
+    else:
+        image_name = sys.argv[1]
+        image_path = os.path.abspath(image_name)
+        image = read_image(image_path).float() / 255
+        image = image.unsqueeze(0)
+        if image.shape[1] >= 3:
+            image = image[:, 0:3, :, :]
+        image = image.to(device)
 
     image_height, image_width = image.shape[2:4]
 
     pretrained_weights = VGG19_Weights.DEFAULT
     pretrained_model = vgg19(weights=pretrained_weights)
 
-    # feature_layers = [5, 10, 19, 28]
-    feature_layers = [28]
-
-    model = Model(pretrained_model, feature_layers)
+    final_layer = 28
+    model = Model(pretrained_model, final_layer)
 
     preprocess = pretrained_weights.transforms()
     preprocess.crop_size = [image_height, image_width]
     preprocess.resize_size = [image_height, image_width]
 
-    p_std = torch.tensor(preprocess.std)
-    p_mean = torch.tensor(preprocess.mean)
-    p_std = torch.reshape(p_std, (1, 3, 1, 1)).to(device)
-    p_mean = torch.reshape(p_mean, (1, 3, 1, 1)).to(device)
-
-    upper_clamp = (1 - p_mean) / p_std
-    lower_clamp = -p_mean / p_std
-
-    image = preprocess(image)
-    image.requires_grad = True
-
-    jitter = 32
-    step_size = 0.2
-
-    optimizer = Adam([image], lr=step_size)
-
-    epochs = 10
-    pbar = tqdm(range(1, epochs + 1))
-    for epoch in pbar:
-        shift_y, shift_x = torch.randint(-jitter, jitter + 1, (2,))
-        # with torch.no_grad():
-            # image = torch.roll(image, shifts=(shift_y, shift_x), dims=(2, 3))
-            # image.requires_grad = True
-
-        L = 0
-        features = model(image)
-        for feature in features:
-            L -= torch.mean(feature**2)
-        L.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        with torch.no_grad():
-            # image = torch.roll(image, shifts=(-shift_y, -shift_x), dims=(2, 3))
-            image.clamp_(lower_clamp, upper_clamp)
-
-        pbar.set_postfix({"L": L.item()})
-
-    # Deprocess
-    image = image * p_std + p_mean
-    image = torch.clamp(image, 0, 1)
+    image = deepdream(image, model, preprocess)
 
     output_file_path = os.path.join(os.getcwd(), "DreamOutput")
     if not os.path.isdir(output_file_path):
         os.mkdir(output_file_path)
 
+    image_name = os.path.split(image_name)[-1]
     fp = os.path.join(output_file_path, image_name)
     save_image(image, fp)
 
